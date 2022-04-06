0b377575b0f12629f0de3f4da090de38325031c1
NerfedWizard <loelnelson@gmail.com>
2022-04-06 17:14:51 -0500

    Not sure what to do

--- quote-react/testing-react-app/src/index.js
+++ quote-react/testing-react-app/src/index.js
@@ -1,53 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Box, Container, CssBaseline, Paper, styled } from '@mui/material';
-import SelectVariants from './Component/CategorySelect';
-import RandomQuote from './Component/RandomQuote';
-import AuthorSelect from './Component/AuthorSelect';
-import './App.css';
+import App from './App';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: 'rgba(86, 11, 184, 0.0)',
-    ...theme.typography.body2,
-    padding: theme.spacing(0.5),
-    position: 'relative',
-    justifyContent: 'center',
-    textAlign: 'center',
-    elevation: 5,
-    fontFamily: 'Hurricane',
-    fontSize: 65,
-    square: false,
-    color: 'antiquewhite',
-}));
-const FullscreenBox = styled(Box)(({ theme }) => ({
-    // flex: '1',
-    // justifyContent: 'space-around',
-    display: 'flex',
-    // size: '100vh',
-}));
-
-const PlusOne = () => {
-
-
-    return (
-        // <div display='flex' flex='1' style={{ height: '150vh', margin: 0, padding: 0 }} id='shine-background'>
-        <FullscreenBox id='shine-background' style={{ height: '100vh', margin: 0, padding: 0 }}>
-            <React.StrictMode>
-                <Router>
-                    <Container >
-                        <CssBaseline />
-                        <Item variant='contained'>
-                            It's Time For Quotes.....
-                        </Item>
-                        <Route exact path='/' component={RandomQuote} />
-                        <Route exact path='/category' component={SelectVariants} />
-                        <Route exact path='/author' component={AuthorSelect} />
-                    </Container>
-                </Router>
-            </React.StrictMode>
-        </FullscreenBox>
-        // </div >
-    );
-}
-ReactDOM.render(<PlusOne />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
